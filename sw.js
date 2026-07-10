4dd05c6d8694478391b3659fd0f4ff25d9608364
Ghost-protocol181 <ed.ireton@gmail.com>
2026-07-10 16:38:17 -0700

    Add offline cache and resilient asset loading

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='eddies-basement-v1';
+const CORE=['./','./index.html','./vault-data.js','./previews.js','./favicon.svg','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin===location.origin){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));}});
