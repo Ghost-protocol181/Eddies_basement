31f5554976c42b7151cb3756b6f6e5b719911430
Ghost-protocol181 <ed.ireton@gmail.com>
2026-07-11 08:18:09 -0700

    Add multi-source artwork candidates

---
+++ artwork-data.js
@@ -0,0 +1,64 @@
+window.EDDIE_ARTWORK = {
+  'Fortnite': [
+    'https://cdn2.unrealengine.com/fortnite-og-social-1920x1080-1920x1080-7e6c85c8c15d.jpg',
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://www.fortnite.com/'
+  ],
+  'Roblox': [
+    'https://images.rbxcdn.com/076d146437ffc79cebc36bf2ded91196.png',
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://www.roblox.com/'
+  ],
+  'Rocket League': [
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/252950/header.jpg',
+    'https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/252950/header.jpg',
+    'https://steamcdn-a.akamaihd.net/steam/apps/252950/header.jpg'
+  ],
+  'Valorant': [
+    'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt4709bcfb012b7075/5eb26f54402b8b4d13a56656/agent-background-generic.JPG',
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://playvalorant.com/'
+  ],
+  'League of Legends': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://www.leagueoflegends.com/',
+    'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt02dd76c15eeda4a9/63ea2b2eb1f08b45b26c1ee5/lol-logo-rendered-hi-res.png'
+  ],
+  'Skribbl.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://skribbl.io/'],
+  'Gartic Phone': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://garticphone.com/'],
+  'Codenames Online': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://codenames.game/'],
+  'Board Game Arena': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://boardgamearena.com/'],
+  'Pokemon Showdown': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://play.pokemonshowdown.com/'],
+  'Chess.com': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://www.chess.com/play/online'],
+  'Lichess': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://lichess.org/'],
+  'Agar.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://agar.io/'],
+  'Slither.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://slither.io/'],
+  'Diep.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://diep.io/'],
+  'Krunker': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://krunker.io/',
+    'https://krunker.io/img/logo_1.png'
+  ],
+  'Shell Shockers': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://shellshock.io/',
+    'https://shellshock.io/img/shellshockers_logo.png'
+  ],
+  '1v1.LOL': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://1v1.lol/',
+    'https://1v1.lol/splash.png'
+  ],
+  'Smash Karts': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://smashkarts.io/',
+    'https://smashkarts.io/images/logo.png'
+  ],
+  'TETR.IO': [
+    'https://image.thum.io/get/width/960/crop/540/noanimate/https://tetr.io/',
+    'https://tetr.io/res/logo.png'
+  ],
+  'Bonk.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://bonk.io/'],
+  'Territorial.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://territorial.io/'],
+  'Colonist': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://colonist.io/'],
+  'PlayingCards.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://playingcards.io/'],
+  'AirConsole': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://www.airconsole.com/'],
+  'JKLM.fun BombParty': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://jklm.fun/'],
+  'Gartic.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://gartic.io/'],
+  'ZombsRoyale.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://zombsroyale.io/'],
+  'Little Big Snake': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://littlebigsnake.com/'],
+  'Starblast.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://starblast.io/'],
+  'Hordes.io': ['https://image.thum.io/get/width/960/crop/540/noanimate/https://hordes.io/']
+};
